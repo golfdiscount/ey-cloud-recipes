f414ef94a13696830fb6bd33eec90cd527deb6a7
Kevin DeGraaf <khdegraaf@gmail.com>
2012-12-19 11:39:16 -0800

    Bump cpu load warning for all instances

--- cookbooks/collectd/recipes/default.rb
+++ cookbooks/collectd/recipes/default.rb
@@ -4,11 +4,7 @@
 #
 
 collectd do
-  if node[:name] == 'rake'
-    load :warning => 5, :failure => 8
-  else
-    load :warning => 4, :failure => 8
-  end
+  load :warning => 5, :failure => 8
   db_space ['1.3GB', '500MB']
   data_space [3000000000, 1500000000]
   root_space :warning => '1245MB', :failure => '500MB'
