0c55d9bf29f53b61d3a0bdf1c5c2013b9a253e52
Kevin DeGraaf <khdegraaf@gmail.com>
2013-02-14 15:27:23 -0800

    Update cron, push inventory every 30 minutes, remove Amazon CA.

--- cookbooks/cron/recipes/default.rb
+++ cookbooks/cron/recipes/default.rb
@@ -61,8 +61,8 @@ if node[:name] == 'rake'
 
   cron "Mercent Inventory Feed" do
     #0 */2 * * *
-    minute  "0"
-    hour    '*/2'
+    minute  "*/30"
+    hour    '*'
     day     '*'
     month   '*'
     weekday '*'
@@ -101,94 +101,6 @@ if node[:name] == 'rake'
     weekday '*'
     user 'deploy'
     command "cd /data/GolfDiscount/current && RAILS_ENV=production rake mybuys:upload_all"
-  end
-
-  cron "Read Amazon Order CA" do
-    #*/30 * * * *
-    minute  "*/30"
-    hour    '*'
-    day     '*'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:read_amazon_order_ca"
-  end
-
-  cron "Write Amazon Order Ack" do
-    #0 */1 * * *
-    minute  "0"
-    hour    '*/1'
-    day     '*'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:send_order_ack"
-  end
-
-  cron "Write Amazon Order Charge" do
-    #0 22 */1 * *
-    minute  "0"
-    hour    '22'
-    day     '*/1'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:send_order_fulfillments"
-  end
-
-  cron "Write Amazon Feeds daily CA" do
-    #0 10 */1 * *
-    minute  "0"
-    hour    '10'
-    day     '*/1'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:send_amazon_ca_feeds"
-  end
-
-  cron "Write Amazon Feeds nightly CA" do
-    #0 1 */1 * *
-    minute  "0"
-    hour    '1'
-    day     '*/1'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:send_amazon_ca_feeds"
-  end
-
-  cron "Write Amazon Product Feed daily CA" do
-    #0 9 */1 * *
-    minute  "0"
-    hour    '9'
-    day     '*/1'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:send_product_ca_feed"
-  end
-
-  cron "Write Amazon Product Feed nightly CA" do
-    #0 0 */1 * *
-    minute  "0"
-    hour    '0'
-    day     '*/1'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:send_product_ca_feed"
-  end
-
-  cron "Read Amazon Processing Reports" do
-    #4 */2 * * *
-    minute  "4"
-    hour    '*/2'
-    day     '*'
-    month   '*'
-    weekday '*'
-    user 'deploy'
-    command "cd /data/GolfDiscount/current && RAILS_ENV=production rake amazon:read_processing_reports"
   end
 
 end
